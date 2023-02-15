51ef587d38beaca271c65e169e4bc35164a4249a
josphanai0203 <115264959+josphanai0203@users.noreply.github.com>
2023-02-15 20:47:54 +0700

    15/2

---
+++ BaiTap/htmls/demo/main.js
@@ -0,0 +1,19 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+let tabs = $$(".tab-item");
+let tabContent = $$(".tab-pane");
+let tabActive = $(".tab-item.active");
+let line = $(".tabs .line");
+line.style.width = tabActive.offsetWidth + "px";
+tabs.forEach((tab,index) => {
+  tab.addEventListener("click", () => {
+  tabs.forEach(tab => tab.classList.remove("active"));
+  tab.classList.add("active");
+
+  tabContent.forEach(tabContent => tabContent.classList.remove("active"));
+  tabContent[index].classList.add("active");
+  line.style.left = tab.offsetLeft + "px";
+  line.style.width = tab.offsetWidth + "px";
+  });
+})
