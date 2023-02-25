f989f8109c13f96aeaa977edfe1a4bff634e419a
josphanai0203 <115264959+josphanai0203@users.noreply.github.com>
2023-02-25 19:58:09 +0700

    bouncing game

--- BaiTap/htmls/case study/main.js
+++ BaiTap/htmls/case study/main.js
@@ -1,65 +1,115 @@
-const $ =document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 
-const boardBox = $('.broadGame');
-const ballBox = $('.ball');
-const barBox = $('.barGame');
+const boardBox = $(".broadGame");
+const ballBox = $(".ball");
+const barBox = $(".gameBar");
 
-let board = new BoardGame(500,500);
-let ballGame = new Ball(0,0,0,1);
-let barGame = new Bar(100,0);
-
-function BoardGame(width, height){
-    this.width = width;
-    this.height = height;
+let boardGame = new Board(500, 500);
+let ballGame = new Ball(0, 50, 0, 2);
+let gameBar = new Bar(100, 0);
+let velocityX = 1;
+let velocityY = 1;
+let speedIncrease = 0.5;
+//Create  BoardGame object
+function Board(width, height) {
+  this.width = width;
+  this.height = height;
 }
-function Ball(x,y,conner,rate){
-    this.x = x;
-    this.y = y;
-    this.conner = conner;
-    this.rate = rate;
-    this.remove = function(){
-
+//Create  BallGame object
+function Ball(x, y, conner, rate) {
+  this.x = x;
+  this.y = y;
+  this.conner = conner;
+  this.rate = rate;
+  //remove function from position X and Y
+  this.remove = function () {
+    this.x += velocityX;
+    this.y += velocityY;
+  };
+  this.changeDirectory = function () {
+    //handle ball when ball touch border left or border right
+    if (this.x < 0 || this.x > boardGame.width - 20) {
+      velocityX = -velocityX;
     }
-    this.changeDirectory = function(){};
+    //handle ball when ball touch border top
+    if (this.y < 0) {
+      velocityY = -velocityY;
+    }
+    //handle ball when ball touch game bar
+    if (this.x >= gameBar.x && this.x <= gameBar.x + 80) {
+      if (this.y >= 360 && this.y < 380) {
+        velocityY = -velocityY;
+      }
+    }
+
+    drawBall();
+  };
 }
-function Bar(width,x){
-    this.width = width;
-    this.x = x;
-    this.turnLeft = function(){};
-    this.turnRight = function(){};
+//Create  GameBar object
+function Bar(width, x) {
+  this.width = width;
+  this.x = x;
+  this.turnLeft = function () {
+    this.x -= 10;
+    if (this.x >= 0 && this.x <= 400) {
+      drawGameBar();
+    } else if (this.x < 0) {
+      this.x = 0;
+      drawGameBar();
+    }
+  };
+  this.turnRight = function () {
+    this.x += 10;
+    if (this.x >= 0 && this.x <= 400) {
+      drawGameBar();
+    }
+  };
 }
-function drawBall(){
-    ballBox.style.left = `${ballGame.x}px`;
-    ballBox.style.top = `${ballGame.y}px`;
-    ballBox.style.width = `20px`;
-    ballBox.style.height = `20px`;
+//create position X of ball random when game start
+function positionBallRandomX() {
+  ballGame.x = Math.floor(Math.random() * 400 + 10);
 }
-function drawBar(){
-    barBox.style.left = `${barGame.x}px`;
-    barBox.style.width = `${barGame.width}px`;
+//draw ball from position X,Y
+function drawBall() {
+  ballBox.style.left = `${ballGame.x}px`;
+  ballBox.style.top = `${ballGame.y}px`;
 }
-function onloadGame(){
-    boardBox.style.width = `${board.width}px`;
-    boardBox.style.height = `${board.height}px`;
-    drawBall()
-    drawBar()
+//draw game bar form width and position X
+function drawGameBar() {
+  barBox.style.left = `${gameBar.x}px`;
+  barBox.style.width = `${gameBar.width}px`;
+}
+//create board game
+function createBoardGame() {
+  boardBox.style.width = `${boardGame.width}px`;
+  boardBox.style.height = `${boardGame.height}px`;
+}
+function CreateBtnPlay(){
+    boardBox.innerHTML = `
+    <div class="ball"></div>
+    <button class="btn">Play</button>
+    <div class="barGame"></div>
+    `;
+}
+//start game when press play button
+function startGame() {
+  let id = setInterval(() => {
+    gameBar.idGame = id;
+    ballGame.changeDirectory();
+    ballGame.remove();
+    if (ballGame.y > 480) {
+      alert("Game Over!");
+      clearInterval(id);
+    }
+  }, 10);
+}
+//load game
+function onloadGame() {
+  createBoardGame();
+  positionBallRandomX();
+  drawBall();
+  drawGameBar();
+  CreateBtnPlay();
+  startGame();
 }
 onloadGame();
-boardBox.onmousemove = function(e){
-    if(e.target != barBox){
-        if (e.layerX>=0&&e.layerX<=400) {
-            barGame.x = e.layerX;
-            
-        }
-
-    }else if(e.target == barBox){
-        if (barGame.x>=0&& (barGame.x+ e.offsetX)<=400) {
-            barGame.x =barGame.x+ e.offsetX;
-            
-        }
-
-    }
-    drawBar()
-}
-
-console.log([barBox])
