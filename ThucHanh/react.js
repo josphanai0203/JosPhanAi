04aedf44a04ba1514e760025b9cdd585002f2fd5
josphanai0203 <115264959+josphanai0203@users.noreply.github.com>
2023-03-03 21:25:43 +0700

    update

--- ThucHanh/react.js
+++ ThucHanh/react.js
@@ -5,13 +5,9 @@ h1.innerHTML= 'Hello 123';
 
 
 
-const h2 = React.createElement('h2',{
-    title: "react 2",
-    className : "heading",
-},React.createElement("h2",{
-    className : "heading2",
-},"1234"),React.createElement("h2",{
-    className : "heading2",
-},"1234"));
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(h2);
+const h2 = <ul>
+    <li className="hidden">hello</li>
+    <li>bcsauch</li>
+</ul>
+const root = document.querySelector("#root")
+ReactDOM.render(h2,root);
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+app.use(morgan("combined"))
 const port = 3000;
 app.get('/', (req, res) => res.send("hello world!"));
 app.listen(port,()=>console.log("example listening"))
