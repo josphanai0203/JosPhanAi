921a5c2f90691e1d5c63587a8d324e7af3410140
josphanai0203 <115264959+josphanai0203@users.noreply.github.com>
2023-03-06 20:49:10 +0700

    he

---
+++ ThucHanh/link.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct Node(){
+    int index;
+    int data;
+    Node next;
+
+}
+void insertNodeN(int index,int data){
+
+}
+int main(){
+    Node head = NULL;
+    Node tail = NULL;
+    Node curr = NULL;
+
+}
--- ThucHanh/react.js
+++ ThucHanh/react.js
@@ -1,13 +1,111 @@
-const h1 = document.createElement('h1');
-h1.title = 'hello world'
-h1.className = "heading"
-h1.innerHTML= 'Hello 123';
+const root = document.querySelector('#root');
+const coursers = [
+    {
+      "id": 15,
+      "title": "HTML CSS Pro",
+      "slug": "htmlcss",
+      "description": "Từ cơ bản tới chuyên sâu, thực hành 8 dự án, hàng trăm bài tập, trang hỏi đáp riêng, cấp chứng chỉ sau khóa học và mua một lần học mãi mãi.",
+      "image": "courses/15/62f13d2424a47.png",
+      "icon": "courses/15/62385d6c63dfa.png",
+      "video_type": "upload",
+      "video": null,
+      "old_price": 2499000,
+      "price": 1299000,
+      "pre_order_price": 699000,
+      "students_count": 2208,
+      "is_pro": true,
+      "is_coming_soon": false,
+      "is_selling": true,
+      "published_at": "2022-08-18T17:00:00.000000Z",
+      "is_registered": false,
+      "user_progress": 0,
+      "last_completed_at": null,
+      "image_url": "https://files.fullstack.edu.vn/f8-prod/courses/15/62f13d2424a47.png",
+      "icon_url": "https://files.fullstack.edu.vn/f8-prod/courses/15/62385d6c63dfa.png",
+      "video_url": "",
+      "landing_page_url": "/landing/htmlcss",
+      "is_pre_order": false,
+      "is_published": true
+    },
+    {
+      "id": 19,
+      "title": "JavaScript Pro",
+      "slug": "javascript-pro",
+      "description": "Khóa học JavaScript Pro",
+      "image": "courses/19/62f13cb607b4b.png",
+      "icon": "courses/19/62f13cb685c81.png",
+      "video_type": "upload",
+      "video": null,
+      "old_price": 0,
+      "price": 0,
+      "pre_order_price": 0,
+      "students_count": 0,
+      "is_pro": true,
+      "is_coming_soon": true,
+      "is_selling": false,
+      "published_at": "2023-01-31T17:00:00.000000Z",
+      "is_registered": false,
+      "user_progress": 0,
+      "last_completed_at": null,
+      "image_url": "https://files.fullstack.edu.vn/f8-prod/courses/19/62f13cb607b4b.png",
+      "icon_url": "https://files.fullstack.edu.vn/f8-prod/courses/19/62f13cb685c81.png",
+      "video_url": "",
+      "landing_page_url": "/landing/javascript-pro",
+      "is_pre_order": false,
+      "is_published": true
+    },
+    {
+      "id": 20,
+      "title": "ReactJS Pro",
+      "slug": "reactjs-pro",
+      "description": "Khóa học ReactJS Pro",
+      "image": "courses/20/62f13dded314e.png",
+      "icon": "courses/20/62f13ddf5e7f9.png",
+      "video_type": "upload",
+      "video": null,
+      "old_price": 0,
+      "price": 0,
+      "pre_order_price": 0,
+      "students_count": 0,
+      "is_pro": true,
+      "is_coming_soon": true,
+      "is_selling": false,
+      "published_at": "2023-05-31T17:00:00.000000Z",
+      "is_registered": false,
+      "user_progress": 0,
+      "last_completed_at": null,
+      "image_url": "https://files.fullstack.edu.vn/f8-prod/courses/20/62f13dded314e.png",
+      "icon_url": "https://files.fullstack.edu.vn/f8-prod/courses/20/62f13ddf5e7f9.png",
+      "video_url": "",
+      "landing_page_url": "/landing/reactjs-pro",
+      "is_pre_order": false,
+      "is_published": false
+    }
+  ]
+  function PostItem(key) {
+    return (
+        <div className="post-item">
+            <img src = {key.imageUrl}/>
+            <h2>{key.title}</h2>
+            <p>{key.description}</p>
+        </div>
+    )
+  }
+  var postArray = coursers.map((courser) => {
+    return (
+        <PostItem title = {courser.title}
+        description ={courser.description}
+        imageUrl = {courser.image_url}
+        key = {courser.id}
+         />
+    )
+  })
+  function App(){
+    return (
+        <div id="wrapper">
+           {postArray}
 
-
-
-const h2 = <ul>
-    <li className="hidden">hello</li>
-    <li>bcsauch</li>
-</ul>
-const root = document.querySelector("#root")
-ReactDOM.render(h2,root);
+        </div>
+    )
+}
+ReactDOM.render(<App />,root)
